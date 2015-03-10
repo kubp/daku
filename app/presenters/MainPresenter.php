1df0fefab0c62707a6ffa23e93d04482ec88f5b6
kubp <dolezal000@gmail.com>
2015-03-10 20:16:00 +0100

    Todo list. MainPresenter added.

---
+++ app/presenters/MainPresenter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette,
+    App\Model;
+
+
+/**
+ * Homepage presenter.
+ */
+class MainPresenter extends BasePresenter
+{
+
+    public function renderDefault()
+    {
+        //$this->template->text = 'any value';
+    }
+
+}
